61c58291740ad16b8c505b23f197a86471dd05d3
Jisan Zaman <jzthegreat@gmail.com>
2014-04-29 02:26:23 -0400

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+ProjectLassie::Application.config.secret_key_base = '5b117b20482fd98ce767881452fad3ed32166b61a748a34ccccb27a8634a5248b0c8041a25b17b81c37361300aa55e16401b363c27bc33fd57f07517cce9b8e1'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+ProjectLassie::Application.config.session_store :cookie_store, key: '_Project_Lassie_session'
